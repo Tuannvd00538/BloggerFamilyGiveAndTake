46585bac28bbf42d089080221cf13c9a65ab8dde
Ngo Van Tuan <ngovantuan@Ngos-MacBook-Pro.local>
2018-07-04 11:08:36 +0700

    Dm Thang Phuong

--- index.js
+++ index.js
@@ -64,32 +64,36 @@ function removeDuplicateUsingFilter(arr){
 // Nhắc hashtag
 
 setInterval(() => {
-    request('https://graph.facebook.com/v3.0/' + idGroup + '/feed?fields=id,message,created_time,from&limit=10&access_token=' + tokenFacebook, (error, response, body) => {
-        if (error) console.log(error);
-        json = JSON.parse(body);
-        if (!json.error) {
+    try {
+        request('https://graph.facebook.com/v3.0/' + idGroup + '/feed?fields=id,message,created_time,from&limit=10&access_token=' + tokenFacebook, (error, response, body) => {
+            if (error) console.log(error);
+            json = JSON.parse(body);
+            if (!json.error) {
 
-            for (var i = 0; i < json.data.length; i++) {
-                try {
-                    message = json.data[i].message;
-                    let checkHashtag = message.replace(/\n/g, '');
-                    let hashtag = new RegExp("#BFG_");
-                    
-                    idPost = json.data[i].id;
-                    idUserPost = json.data[i].from.id;
-                    if (message != undefined && message != null && !hashtag.test(checkHashtag.toUpperCase()) && !remindedID.hasOwnProperty(idPost)) {
-                        var comment = "Bạn @[" + idUserPost + ":0] ơi, bạn đã đọc nội quy chưa vậy. Thiếu HASHTAG rồi kìa: https://bit.ly/2KiHuPR 3:) >:(";
-                        post_comment(comment, idPost);
-                        console.log("Nhắc nhở bài viết: https://www.facebook.com/" + idPost);
-                        remindedID[idPost] = true;
-                        break;
+                for (var i = 0; i < json.data.length; i++) {
+                    try {
+                        message = json.data[i].message;
+                        let checkHashtag = message.replace(/\n/g, '');
+                        let hashtag = new RegExp("#BFG_");
+                        
+                        idPost = json.data[i].id;
+                        idUserPost = json.data[i].from.id;
+                        if (message != undefined && message != null && !hashtag.test(checkHashtag.toUpperCase()) && !remindedID.hasOwnProperty(idPost)) {
+                            var comment = "Bạn @[" + idUserPost + ":0] ơi, bạn đã đọc nội quy chưa vậy. Thiếu HASHTAG rồi kìa: https://bit.ly/2KiHuPR 3:) >:(";
+                            post_comment(comment, idPost);
+                            console.log("Nhắc nhở bài viết: https://www.facebook.com/" + idPost);
+                            remindedID[idPost] = true;
+                            break;
+                        }
+                    } catch (error) {
+                        console.log('error', error);
                     }
-                } catch (error) {
-                    console.log('error', error);
                 }
             }
-        }
-    });
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }, 5000);
 
 
