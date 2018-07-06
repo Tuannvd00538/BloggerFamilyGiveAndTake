737e587a4dde3f82059cf8627bff36a9cc00a148
Ngo Van Tuan <ngovantuan@Ngos-MacBook-Pro.local>
2018-07-06 19:34:34 +0700

    update

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const express = require('express');
 const fs = require("fs");
 const request = require('request');
-const tokenFacebook = "EAAAAUaZA8jlABACUWa8MMssPceglG8EBcXTs7MZCAzlaEHUhSZA0SyfahGYlgTf25Ee3WU3ux2k4U8TFArCKMjbZC0WZA3xLl0Fq14EQlGlDZC1KPIXh33k27tQHscwPo1IXY6n6FTlewoZAq4rCUCzUb04PMgxGzeHUq4hGDtISgZDZD";
+const tokenFacebook = "";
 const idGroup = "614878965530147";
 var remindedID = {};
 var message, idPost, idUserPost;
-
-function checkUser(id) {
-    var count = 0;
-    listUser.forEach(function (value) {
-        if (id == value) {
-            count++;
-        }
-    });
-    if (count != 0) {
-        return false;
-    } else {
-        return true;
-    }
-}
 
 async function post_comment(contentComment, idPost) {
     if (contentComment.trim() != "")
@@ -31,34 +17,6 @@ async function post_comment(contentComment, idPost) {
 
         });
 
-}
-
-async function delete_member(id) {
-    await request('https://graph.facebook.com/' + idGroup + '/members?method=delete&member=' + id + '&access_token=' + tokenFacebook, (error, response, body) => {
-        if (error) console.log(error);
-        var data = JSON.parse(body);
-        return data;
-    });
-}
-
-async function checkLog(id) {
-    await request('https://graph.facebook.com/v3.0/' + id + '/comments?access_token=' + tokenFacebook, (error, response, body) => {
-        if (error) console.log(error);
-        json = JSON.parse(body);
-        if (!json.error) {
-            let cmt = json.data[j].message;
-            let reg = new RegExp(id, "g");
-            let result = cmt.match(reg);
-            return result == null;
-        }
-    });
-}
-
-function removeDuplicateUsingFilter(arr){
-    let unique_array = arr.filter(function(elem, index, self) {
-        return index == self.indexOf(elem);
-    });
-    return unique_array
 }
 
 // Nhắc hashtag
@@ -95,60 +53,6 @@ setInterval(() => {
         console.log(error);
     }
 }, 5000);
-
-
-// Block chấm, hóng
-var listPost = listUser = listCmt = [];
-var commentBlock = ".|,|:|;|..|chấm|hóng";
-var block = commentBlock.split("|");
-setInterval(() => {
-    try {
-        request('https://graph.facebook.com/v3.0/' + idGroup + '/feed?fields=id,message,created_time,from&limit=10&access_token=' + tokenFacebook, (error, response, body) => {
-            if (error) console.log(error);
-            json = JSON.parse(body);
-            if (!json.error) {
-                for (var i = 0; i < json.data.length; i++) {
-                    let idPost = json.data[i].id;
-                    listPost.push(idPost);
-                };
-            }
-        });
-        listPost = removeDuplicateUsingFilter(listPost);
-        if(listPost.length >= 100) {
-            listPost = [];
-        }
-        if (listPost.length != 0) {
-            for(var i = 0; i < listPost.length; i++) {
-                request('https://graph.facebook.com/v3.0/' + listPost[i] + '/comments?access_token=' + tokenFacebook, (error, response, body) => {
-                    if (error) console.log(error);
-                    json = JSON.parse(body);
-                    if (!json.error && json.length != 0) {
-                        for (var j = 0; j < json.data.length; j++) {
-                            var cmt = json.data[j].message;
-                            var idUserCmt = json.data[j].from.id;
-                            var nameUserCmt = json.data[j].from.name;
-                            block.forEach(function (value) {
-                                if (cmt.toUpperCase() == value.toUpperCase() && checkUser(idUserCmt)) {
-                                    if (checkLog(idUserCmt)) {
-                                        console.log('blockUser', idUserCmt);
-                                        var cmtLog = "Kick User '" + nameUserCmt + " (fb.com/" + idUserCmt + ")' | Comment '" + cmt + "'";
-                                        if (delete_member(idUserCmt)) {
-                                            post_comment(cmtLog, '614878965530147_677883022563074');   
-                                        }   
-                                    }
-                                }
-                            });
-                            break;
-                        }
-                    }
-                });
-            }
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}, 30000);
-
 
 var app = express();
 app.use(express.static('./public'));
